a81694221519404c1921ad5884c04724cc1a72f5
markush97 <markus@hinkel.me>
2021-05-07 21:15:39 +0200

    refactor(api): move function from wrong file
    getByIdForReport(..) which belongs to the CheckResultRepository was
    in the BasicRepository where it does not belong

--- libs/api/checks/src/lib/persistence/check-result.repository.ts
+++ libs/api/checks/src/lib/persistence/check-result.repository.ts
@@ -1,8 +1,12 @@
 import { EntityRepository } from "typeorm";
 import { CheckResultEntity } from "./check-result.entity";
 import { BasicRepository } from '@system4blue/api/crud';
+import { UUID4 } from "@system4blue/types";
 
 @EntityRepository(CheckResultEntity)
 export class CheckResultRepository extends BasicRepository<CheckResultEntity> {
 
+  async getByIdForReport(id: UUID4): Promise<CheckResultEntity> {
+    return this.repository.findOne(id, {relations: ['template', 'checker', 'responsible', 'checkResults', 'checkResults.item']});
+  }
 }
